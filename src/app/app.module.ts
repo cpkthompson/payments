a1f2723c58030f3f17067ffa953e82fb4f2e58cb
Charles Thompson <cpkthompson@gmail.com>
2021-03-03 15:35:35 +0000

    create a number of components and a module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { ResultsComponent } from './results/results.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    ResultsComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/case/case.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CaseComponent } from './case.component';
+
+
+
+@NgModule({
+  declarations: [CaseComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class CaseModule { }
